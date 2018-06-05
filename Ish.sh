4c257b53852926f54dbc570f5dadae7e2c48811c
Droid <39285561+droidxerror123@users.noreply.github.com>
2018-06-05 04:28:16 +0000

    Create Ish.sh

---
+++ Ish.sh
@@ -0,0 +1,98 @@
+#!/system/xbin/bash
+#Hello-Word
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay "ISHSPAM" | lolcat
+echo "" 
+echo "==============================================="     
+echo "\033[32;1m=Author : ./Droid_X_ISH             =" 
+echo "\033[35;1m=Team   : Indonesian Security Hunter="
+echo "\033[33;1m=Kontak gua:  089606402413          ="
+echo "\033[31;1m=Date   :05-06-2018                 ="
+echo "==============================================="
+sleep 1
+echo ""
+echo "\033[32;1mSilahkan dipilih:"
+echo "\033[36;1m"
+echo "====================================="
+echo $green  "=1.> Spam Sms Tokopedia     ="
+echo $purple "=2.> Spam Sms Telkomnyet    ="
+echo $cyan   "=3.> Spam Sms Matahari Mall ="
+echo $red    "=4.> Spam Sms HooqTV        ="
+echo $white  "=5.> Spam Sms KFC           ="
+echo $green  "=6.> Spam Sms PHD           ="
+echo "====================================="
+echo "\033[32;1m"
+read -p "[==>ISHSPAM:~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Mr.Kuro Cyber" | lolcat
+cd module
+php tp.php
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "./Droid_X_ISH"
+cd module
+php t.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+figlet "./Droid_X_ISH"
+cd module
+php mataharimall.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet "./Droid_X_ISH"
+cd module
+php hooq.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f slant --gay "./Droid_X_ISH"
+cd module
+php kfc.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f mono12 -F gay "Tn.Chaplin"
+cd module
+php phd.php
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mINDONESIA SECURITY HUNTER"
+sleep 1
+echo "\033[33;1mWe Will Hunt Every Gap!!"
+sleep 1
+echo " Family:"
+sleep 1
+echo "Anonymous Jember"
+sleep 1
+echo "Indonesia Fighter Cyber"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
